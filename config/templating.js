83306d7dbf9c7e86b10fce6d55b5c9f261602cda
Trav Colbert <trav.colbert@gmail.com>
2025-05-22 16:58:42 -0600

    layered templating

--- config/templating.js
+++ config/templating.js
@@ -1,7 +1,22 @@
+const fs = require('fs')
 const path = require('path')
 
 module.exports = function (app) {
     app.set('view options', { compileDebug: false })
-    app.set('views', path.join(__dirname, '/..', app.locals.appPath, app.locals.viewPath))
+
+    let viewPaths = app.locals.appList.split(',').map(appInstance => {
+        const viewPath = path.join(__dirname, '../', appInstance.trim(), app.locals.viewPath)
+        if (fs.existsSync(viewPath)) {
+            return viewPath
+        }
+        return null
+    }).filter(viewPath => viewPath)
+
+    if (fs.existsSync(path.join(__dirname, '..', app.locals.viewPath))) {
+        viewPaths = viewPaths.concat(path.join(__dirname, '..', app.locals.viewPath))
+    }
+
+    app.set('views', viewPaths)
+
     app.set('view engine', 'pug')
 }
