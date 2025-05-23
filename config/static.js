7863d38f938c40ca62c57f9d4e3ef26f0ca67696
Trav Colbert <trav.colbert@gmail.com>
2025-05-23 11:01:59 -0600

    improved comment

--- config/static.js
+++ config/static.js
@@ -9,7 +9,10 @@ module.exports = function (app) {
       app.locals.debug && console.debug(`Configuring default static files location for: ${appInstance} - ${location}`)
       app.use(express.static(location))
     }
-    // load app-level config
+    /**
+     * There might be additional, app-specific static locations.
+     * This allows us to ask the app to configure its own static files.
+     */
     const appConfig = path.join(__dirname, '../', appInstance.trim(), 'config', 'static.js')
     if (fs.existsSync(appConfig)) {
       require(appConfig)(app)
